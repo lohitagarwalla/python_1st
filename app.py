7bf46d568ee635834d71a5c326d9e6ae738a7207
lohitagarwalla <47322332+lohitagarwalla@users.noreply.github.com>
2019-08-28 23:24:44 +0530

    my first comit from pycharm

--- app.py
+++ app.py
@@ -4,4 +4,3 @@ new_string = string[1:12]
 another = string[:] #copy
 print(another)
 print(new_string)
-
---
+++ strings.py
@@ -0,0 +1,2 @@
+new = ''' this is a tripple quoted sentence, here we can use use both single and double quote inside it 'fds "fds "fds ' '''
+print(new)
